a2cc200c818864ea0a4e8f8f5040863989a3a021
Dhanush Kiran K <dhanuu08@gmail.com>
2020-03-24 20:37:52 +0530

    increase and decrease quantitty functionality added

--- React/cart/src/CartItem.js
+++ React/cart/src/CartItem.js
@@ -9,10 +9,28 @@ class CartItem extends React.Component{
             Quantity : 1,
             img : ''
         }
-        // this.increaseQuantity = this.increaseQuantity.bind(this);
+        // this.increaseQuantity = this.increaseQuantity.bind(this); - if we don't use arrow function use this to pass "this" to the function
     }
     increaseQuantity = () =>{
-        console.log(this.state);
+        
+        //inherited from react.component, which tells react to render the changes in state variables
+        // this.setState({//method 1 - object form
+        //     Quantity : this.state.Quantity+=1
+        // })
+
+        this.setState((prevState) => {//method2 function form - when previous state is required
+            return{
+                Quantity : prevState.Quantity+1
+            }
+        })
+    }
+
+    decreaseQuantity =() =>{
+        this.setState((prevState) => {
+            return {
+                Quantity : prevState.Quantity-1
+            }
+        })
     }
     render(){
         const {title,price,Quantity} = this.state;
@@ -36,7 +54,8 @@ class CartItem extends React.Component{
 
                         <img alt="decrease" 
                         className="action-icons" 
-                        src="https://image.flaticon.com/icons/svg/463/463700.svg" />
+                        src="https://image.flaticon.com/icons/svg/463/463700.svg"
+                        onClick={ this.decreaseQuantity } />
 
                         <img alt="delete" 
                         className="action-icons" 
